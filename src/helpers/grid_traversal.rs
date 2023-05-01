98844a862fe0361aa5a82dba5fa2e4227ad313b5
LucasBrunner <lucasbrunner@gmail.com>
2023-05-01 12:23:54 -0400

    expand grid traversal tests

--- src/helpers/grid_traversal.rs
+++ src/helpers/grid_traversal.rs
@@ -65,6 +65,30 @@ mod grid_traversal_tests {
   use super::GridTraversal;
 
   #[test]
+  fn zero_traversal() {
+    let mut traverser = GridTraversal::new(IVec2::ZERO, IVec2::ZERO);
+    assert_eq!(traverser.next(), None);
+  }
+
+  #[test]
+  fn zero_extend_one_traversal() {
+    let mut traverser = GridTraversal::new(IVec2::ZERO, IVec2::ZERO).add_iterations(1);
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 0 }));
+    assert_eq!(traverser.next(), None);
+  }
+
+  #[test]
+  fn zero_extend_many_traversal() {
+    let mut traverser = GridTraversal::new(IVec2::ZERO, IVec2::ZERO).add_iterations(5);
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 0 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 0 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 0 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 0 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 0 }));
+    assert_eq!(traverser.next(), None);
+  }
+
+  #[test]
   fn horizontal_traversal() {
     let mut traverser = GridTraversal::new(IVec2::ZERO, IVec2::X * 5);
     assert_eq!(traverser.next(), Some(IVec2 { x: 1, y: 0 }));
@@ -72,6 +96,109 @@ mod grid_traversal_tests {
     assert_eq!(traverser.next(), Some(IVec2 { x: 3, y: 0 }));
     assert_eq!(traverser.next(), Some(IVec2 { x: 4, y: 0 }));
     assert_eq!(traverser.next(), Some(IVec2 { x: 5, y: 0 }));
+    assert_eq!(traverser.next(), None);
+  }
+
+  #[test]
+  fn vertical_traversal() {
+    let mut traverser = GridTraversal::new(IVec2::ZERO, IVec2::Y * 5);
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 1 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 2 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 3 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 4 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 5 }));
+    assert_eq!(traverser.next(), None);
+  }
+
+  #[test]
+  fn diagonal_traversal() {
+    let mut traverser = GridTraversal::new(IVec2::ZERO, IVec2::ONE * 5);
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 1 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 1, y: 1 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 1, y: 2 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 2, y: 2 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 2, y: 3 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 3, y: 3 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 3, y: 4 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 4, y: 4 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 4, y: 5 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 5, y: 5 }));
+    assert_eq!(traverser.next(), None);
+  }
+
+  #[test]
+  fn horizontal_traversal_reverse() {
+    let mut traverser = GridTraversal::new(IVec2::X * 5, IVec2::ZERO);
+    assert_eq!(traverser.next(), Some(IVec2 { x: 4, y: 0 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 3, y: 0 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 2, y: 0 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 1, y: 0 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 0 }));
+    assert_eq!(traverser.next(), None);
+  }
+
+  #[test]
+  fn vertical_traversal_reverse() {
+    let mut traverser = GridTraversal::new(IVec2::Y * 5, IVec2::ZERO);
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 4 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 3 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 2 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 1 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 0 }));
+    assert_eq!(traverser.next(), None);
+  }
+
+  #[test]
+  fn diagonal_traversal_reverse() {
+    let mut traverser = GridTraversal::new(IVec2::ONE * 5, IVec2::ZERO);
+    assert_eq!(traverser.next(), Some(IVec2 { x: 5, y: 4 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 4, y: 4 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 4, y: 3 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 3, y: 3 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 3, y: 2 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 2, y: 2 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 2, y: 1 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 1, y: 1 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 1, y: 0 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: 0 }));
+    assert_eq!(traverser.next(), None);
+  }
+
+  #[test]
+  fn horizontal_traversal_negative() {
+    let mut traverser = GridTraversal::new(IVec2::ZERO, IVec2::NEG_X * 5);
+    assert_eq!(traverser.next(), Some(IVec2 { x: -1, y: 0 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: -2, y: 0 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: -3, y: 0 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: -4, y: 0 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: -5, y: 0 }));
+    assert_eq!(traverser.next(), None);
+  }
+
+  #[test]
+  fn vertical_traversal_negative() {
+    let mut traverser = GridTraversal::new(IVec2::ZERO, IVec2::NEG_Y * 5);
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: -1 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: -2 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: -3 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: -4 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: -5 }));
+    assert_eq!(traverser.next(), None);
+  }
+
+  #[test]
+  fn diagonal_traversal_negative() {
+    let mut traverser = GridTraversal::new(IVec2::ZERO, IVec2::NEG_ONE * 5);
+    assert_eq!(traverser.next(), Some(IVec2 { x: 0, y: -1 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: -1, y: -1 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: -1, y: -2 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: -2, y: -2 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: -2, y: -3 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: -3, y: -3 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: -3, y: -4 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: -4, y: -4 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: -4, y: -5 }));
+    assert_eq!(traverser.next(), Some(IVec2 { x: -5, y: -5 }));
     assert_eq!(traverser.next(), None);
   }
 }
