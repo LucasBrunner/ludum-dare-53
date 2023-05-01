9fb587f789b9448d3e7634af175acb7bc8a09a47
LucasBrunner <lucasbrunner@gmail.com>
2023-05-01 11:40:13 -0400

    remove catch tile change from placement

--- src/conveyor/placement.rs
+++ src/conveyor/placement.rs
@@ -4,16 +4,13 @@ use bevy_ecs_tilemap::{
   tiles::{TileBundle, TilePos, TileStorage, TileTextureIndex},
 };
 
-use crate::{helpers::grid_traversal::GridTraversal, vec2_traits::*, ConveyorDirection, input::{chained_tile::{prelude::*, ChainedTilePlaceDirection}, tile_rotation::SelectedTileDirection,},};
-
-use super::{ConveyorTileLayer, removal::despawn_conveyor};
+use crate::{input::chained_tile::ChainedTilePlaceDirection, vec2_traits::*, ConveyorDirection};
 
 pub mod prelude {
   pub use super::UpdatedTile;
 }
 
 pub mod plugin_exports {
-  pub use super::catch_chained_tile_change_events;
   pub use super::PreviousPlaceTileAttempt;
 }
 
@@ -76,9 +73,19 @@ fn place_tile(
 ) {
   if chain {
     if let Some(previous_tile_pos) = previous_tile.position {
-      if let Some(direction_moved) = ConveyorDirection::from_ivec2(position - previous_tile_pos.as_ivec2()) {
-        if (place_direction == ChainedTilePlaceDirection::Revesed) == (previous_tile.direction == direction_moved) {
-          update_tile_direction(&mut commands, previous_tile_pos, &tile_storage, direction_moved.apply_place_direction(place_direction), &mut placed_tiles);
+      if let Some(direction_moved) =
+        ConveyorDirection::from_ivec2(position - previous_tile_pos.as_ivec2())
+      {
+        if (place_direction == ChainedTilePlaceDirection::Revesed)
+          == (previous_tile.direction == direction_moved)
+        {
+          update_tile_direction(
+            &mut commands,
+            previous_tile_pos,
+            &tile_storage,
+            direction_moved.apply_place_direction(place_direction),
+            &mut placed_tiles,
+          );
         }
         selected_tile_direction = direction_moved;
       }
@@ -98,42 +105,5 @@ fn place_tile(
       position: Some(position),
       direction: selected_tile_direction.apply_place_direction(place_direction),
     };
-  }
-}
-
-pub fn catch_chained_tile_change_events(
-  mut commands: Commands,
-  mut place_tile_events: EventReader<ChainedTileChangeEvent>,
-  mut placed_tiles: EventWriter<UpdatedTile>,
-  mut tilemap: Query<(Entity, &mut TileStorage, &TilemapSize, &ConveyorTileLayer)>,
-  mut previous_tile_attempt: ResMut<PreviousPlaceTileAttempt>,
-  selected_tile_rotation: Res<SelectedTileDirection>,
-) {
-  let Ok((tilemap_entity, mut tile_storage, tilemap_size, _)) = tilemap.get_single_mut() else { 
-    error!(
-      "Tilemap query for the conveyor layer returned {} items when it only should have returned 1.", 
-      tilemap.iter().len(),
-    );
-    return; 
-  };
-
-  for place_tile_event in place_tile_events.iter() {   
-    let positions = match place_tile_event.position {
-      ChainedTileChangePosition::Single(position) => GridTraversal::new(position, position).add_iterations(1),
-      ChainedTileChangePosition::StraightLine { start, end } => GridTraversal::new(start, end),
-    };
-
-    for position in positions {
-      match place_tile_event.change_type {
-        crate::input::chained_tile::ChainedTileChangeType::Put { tile_type: _tile_type, chain, direction } => {
-          place_tile(&mut commands, position, &mut tile_storage, tilemap_entity, tilemap_size, &mut previous_tile_attempt, &mut placed_tiles, direction, selected_tile_rotation.direction, chain);
-        },
-        crate::input::chained_tile::ChainedTileChangeType::Delete => {
-          if let Ok(position) = position.to_tile_pos(&tilemap_size) {
-            despawn_conveyor(&mut commands, position, &mut tile_storage, &mut placed_tiles);
-          }
-        },
-      }
-    }
   }
 }
